b3bee16cd65b42a5dcebe96e141eebafe7bc4bbc
Huy <doxhuy2104@gmail.com>
2025-06-01 17:42:29 +0700

    sqlite

--- app/src/main/java/com/example/studentmanagement/AddStudent.kt
+++ app/src/main/java/com/example/studentmanagement/AddStudent.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class AddStudent : AppCompatActivity() {
+    private lateinit var database: Database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_student)
@@ -22,6 +24,8 @@ class AddStudent : AppCompatActivity() {
             insets
         }
 
+        database = Database(this)
+
         val editName = findViewById<EditText>(R.id.name)
         val editMSSV = findViewById<EditText>(R.id.mssv)
         val editEmail = findViewById<EditText>(R.id.email)
@@ -30,10 +34,17 @@ class AddStudent : AppCompatActivity() {
 
 
         button.setOnClickListener{
-            val studentSize = intent.getIntExtra("studentListSize", 0)
-
+//            val studentSize = intent.getIntExtra("studentListSize", 0)
+            val name = editName.text.toString().trim()
+            val mssv = editMSSV.text.toString().trim()
+            val email = editEmail.text.toString().trim()
+            val phone = editPhone.text.toString().trim()
+            if (database.isMSSVExists(mssv)) {
+                Toast.makeText(this, "MSSV đã tồn tại!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val student = StudentModel(
-                id = studentSize+1,
+                id = 0,
                 editName.text.toString(),
                 editMSSV.text.toString(),
                 editEmail.text.toString(),
